33e8c7bd0bae3cf35010467e2dea2207e7deddb7
lorijr <jrsilva.lorivas@gmail.com>
2025-09-19 08:49:40 -0300

    first commit

---
+++ src/main/java/com/loujunior/appcommongo/AppcommongoApplication.java
@@ -0,0 +1,13 @@
+package com.loujunior.appcommongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcommongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcommongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loujunior/appcommongo/AppcommongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loujunior.appcommongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcommongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
