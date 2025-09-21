00ada8c8d96361696ce7a9230b3aa58d725c9edb
lorijr <jrsilva.lorivas@gmail.com>
2025-09-21 01:35:32 -0300

    obtendo post por id

--- src/main/java/com/loujunior/appcommongo/services/PostService.java
+++ src/main/java/com/loujunior/appcommongo/services/PostService.java
@@ -1,14 +1,21 @@
 package com.loujunior.appcommongo.services;
 
 import com.loujunior.appcommongo.domain.Post;
-import com.loujunior.appcommongo.domain.User;
-import com.loujunior.appcommongo.dto.AuthorDTO;
 import com.loujunior.appcommongo.repository.PostRepository;
+import com.loujunior.appcommongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class PostService {
 
     @Autowired
     private PostRepository repository;
 
+    public Post findById(String id){
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 }
