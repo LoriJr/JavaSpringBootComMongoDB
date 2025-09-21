a223584d82e15a49890975dabd85cf8392fcf3d9
lorijr <jrsilva.lorivas@gmail.com>
2025-09-21 01:38:57 -0300

    realizando get para todos os PostS

--- src/main/java/com/loujunior/appcommongo/resources/PostResource.java
+++ src/main/java/com/loujunior/appcommongo/resources/PostResource.java
@@ -1,6 +1,8 @@
 package com.loujunior.appcommongo.resources;
 
 import com.loujunior.appcommongo.domain.Post;
+import com.loujunior.appcommongo.domain.User;
+import com.loujunior.appcommongo.dto.UserDTO;
 import com.loujunior.appcommongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -21,5 +25,11 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Post>> find(){
+        List<Post> list = service.find();
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/loujunior/appcommongo/services/PostService.java
+++ src/main/java/com/loujunior/appcommongo/services/PostService.java
@@ -1,11 +1,13 @@
 package com.loujunior.appcommongo.services;
 
 import com.loujunior.appcommongo.domain.Post;
+import com.loujunior.appcommongo.domain.User;
 import com.loujunior.appcommongo.repository.PostRepository;
 import com.loujunior.appcommongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +19,9 @@ public class PostService {
     public Post findById(String id){
         Optional<Post> post = repository.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> find(){
+        return repository.findAll();
     }
 }
